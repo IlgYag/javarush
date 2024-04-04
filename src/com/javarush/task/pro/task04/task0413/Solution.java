dd0e25aa1c5011f63fefd2daf9facf2b6f108de2
dasama <ilyagafarov@gmail.com>
2024-04-05 02:05:09 +0500

    javarush

---
+++ src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task04.task0413;
+
+/* 
+Рисуем треугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int rowQuan = 10;
+        int colQuan = 10;
+        for (int i = 0; i < rowQuan; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+            //напишите тут ваш код
+
+    }
+}

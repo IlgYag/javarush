d911e5bb4a7d46558f55fe30981edc3ff650b2e3
dasama <ilyagafarov@gmail.com>
2024-04-05 13:17:30 +0500

    javarush

---
+++ src/com/javarush/task/pro/task04/task0418/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task04.task0418;
+
+import java.util.Scanner;
+
+/* 
+Стакан наполовину пуст или наполовину полон?
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        double glass = 0.5;
+        Scanner scanner = new Scanner(System.in);
+        boolean optimist = scanner.nextBoolean();
+
+        if (optimist) {
+            int rezOpt = (int) (Math.ceil(glass));
+            System.out.println(rezOpt);//напишите тут ваш код
+        } else if (!optimist) {
+            int rezPess = (int) (Math.floor(glass));
+            System.out.println(rezPess);
+        }
+
+    }
+}

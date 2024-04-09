0b18e31cee95a9babbc523eaffd687079f52b39d
dasama <ilyagafarov@gmail.com>
2024-04-09 10:47:13 +0500

    javarush

---
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(int i){
+
+    }
+    public static void universalMethod(int i, int j){
+
+    }
+    public static void universalMethod(int i, int j, int k){
+
+    }
+    public static void universalMethod(String a){
+
+    }
+    public static void universalMethod(String a, String b){
+
+    }
+    public static void universalMethod(String a, String b, String c){
+
+    }
+    public static void universalMethod(boolean logic){
+
+    }
+    public static void universalMethod(boolean logic, int i){
+
+    }
+    public static void universalMethod(boolean logic, String a){
+
+    }
+
+    //напишите тут ваш код
+}

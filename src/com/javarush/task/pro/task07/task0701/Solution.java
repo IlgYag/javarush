27416bb06845f65b5aadd9542f5cca76e9d5f1ba
dasama <ilyagafarov@gmail.com>
2024-04-10 08:58:51 +0500

    javarush

---
+++ src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte byte1Var;
+    short short2Var;
+    int int4Var;
+    long long8Var;
+    float float4Var;
+    double double8Var;
+    boolean boolean1Var;
+    char char2Var;//напишите тут ваш код
+}

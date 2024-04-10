df3b9aa4ecf3bd9f7e461628df4c75405c3124f5
dasama <ilyagafarov@gmail.com>
2024-04-10 08:59:20 +0500

    javarush

---
+++ src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte byte1Var = Byte.MIN_VALUE;
+    short short2Var = Short.MIN_VALUE;
+    int int4Var = Integer.MIN_VALUE;
+    long long8Var = Long.MIN_VALUE;//напишите тут ваш код
+}

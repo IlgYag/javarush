d1cf3180f40d53437f7cddb5456ad3529006be50
dasama <ilyagafarov@gmail.com>
2024-04-10 08:59:07 +0500

    javarush

---
+++ src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte byte1Var = Byte.MAX_VALUE;
+    short short2Var = Short.MAX_VALUE;
+    int int4Var = Integer.MAX_VALUE;
+    long long8Var = Long.MAX_VALUE;//напишите тут ваш код
+}

cb6ded5a446fa751947d05b031b6ff334bf9a2eb
dasama <ilyagafarov@gmail.com>
2024-04-11 17:29:58 +0500

    javarush

---
+++ src/com/javarush/task/pro/task07/task0706/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0706;
+
+/* 
+Странное деление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        double num1 = 0, num2 = 0;
+        Solution.div(num1, num2);//напишите тут ваш код
+    }
+
+    public static void div(double a, double b) {
+        System.out.println(b / a);
+    }
+}

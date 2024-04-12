4cf121f3886d5be10a638860cf8f7aada6e67393
dasama <ilyagafarov@gmail.com>
2024-04-12 16:29:30 +0500

    javarush

---
+++ src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = new String("Привет");
+        String t = new String("дорогой");
+        String r = new String("друг");
+        String i = new String("рад");
+        String n = new String("с");
+        String g = new String("тобой");
+        String st = new String("пообщаться");
+        String ri = new String("ты");
+        String ng = new String("лучший");
+        String string = new String("друг");
+
+        int[] array1 = new int[5];
+        int[] array2 = new int[6];
+        int[] array3 = new int[7];
+        int[] array4 = new int[8];
+        int[] array5 = new int[9];
+
+        Scanner scanner = new Scanner(System.in);
+        Scanner console =new Scanner(System.in);//напишите тут ваш код
+    }
+}

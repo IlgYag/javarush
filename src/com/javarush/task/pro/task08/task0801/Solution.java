cb4143296718d278fa553687cc9dfb045848c426
dasama <ilrudasama@gmail.com>
2024-04-13 11:20:10 +0500

    javarush

---
+++ src/com/javarush/task/pro/task08/task0801/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task08.task0801;
+
+import static java.lang.Math.toRadians;
+
+/* 
+Утильный класс: часть 1
+*/
+
+public class Solution {
+
+    public static double sin(double a) {
+        double result = Math.sin(Math.toRadians(a));//напишите тут ваш код
+        return result;
+    }
+
+    public static double cos(double a) {
+        double result = Math.cos(Math.toRadians(a));//напишите тут ваш код
+        return result;
+    }
+
+    public static double tan(double a) {
+        double result = Math.tan(Math.toRadians(a));//напишите тут ваш код
+        return result;
+    }
+}

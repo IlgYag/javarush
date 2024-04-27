4e63093f54ef4b59f446795199421647c727221d
dasama <ilrudasama@gmail.com>
2024-04-27 11:58:49 +0500

    les 11 task 20

---
+++ src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task10.task1020;
+
+public class Computer {
+    public Computer(){
+        new SystemUnit();
+        new Mouse();
+        new Monitor();
+        new Keyboard();
+    }
+
+}

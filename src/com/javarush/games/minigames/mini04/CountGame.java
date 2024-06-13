d86efb7f41a57eba3ac46b46e5e51640bda74b21
dasama <ilrudasama@gmail.com>
2024-06-14 00:31:08 +0500

    les 20 task 4

---
+++ src/com/javarush/games/minigames/mini04/CountGame.java
@@ -0,0 +1,47 @@
+package com.javarush.games.minigames.mini04;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Считаем клетки
+*/
+
+public class CountGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(10, 10);
+        showGrid(false);
+        for (int j = 0; j < 50; j++) {
+            int x = getRandomNumber(10);
+            int y = getRandomNumber(10);
+            setCellColor(x, y, Color.GREEN);
+            setCellNumber(x, y, getRandomNumber(100));
+        }
+        showResult();
+    }
+
+    public void showResult() {
+        int sum = 0;
+        int cellNum = 0;
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                if (getCellColor(col, row) == Color.GREEN) {
+                    cellNum++;
+                    sum+=getCellNumber(col,row);
+                }
+            }
+        }
+        printSum(sum);
+        printCountOfGreenCells(cellNum);
+    }
+
+    private void printSum(int sum) {
+        System.out.println("Сумма всех чисел = " + sum);
+    }
+
+    private void printCountOfGreenCells(int count) {
+        System.out.println("Количество зеленых клеток = " + count);
+    }
+}

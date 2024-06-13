bdda91ab98ac8d58dbbf6b24940aa92ab5d9bb3b
dasama <ilrudasama@gmail.com>
2024-06-14 00:52:09 +0500

    les 20 task 5

--- src/com/javarush/games/minigames/mini05/RainbowGame.java
+++ src/com/javarush/games/minigames/mini05/RainbowGame.java
@@ -1,13 +1,24 @@
 package com.javarush.games.minigames.mini05;
 
-import com.javarush.engine.cell.Game;
 import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
 
 /* 
 Цвета радуги
 */
 
 public class RainbowGame extends Game {
-
-    //напишите тут ваш код
+    @Override
+    public void initialize() {
+        setScreenSize(10, 7);
+            for (int j = 0; j < 10; j++){
+                setCellColor(j,0, Color.RED);
+                setCellColor(j,1, Color.ORANGE);
+                setCellColor(j,2, Color.YELLOW);
+                setCellColor(j,3, Color.GREEN);
+                setCellColor(j,4, Color.BLUE);
+                setCellColor(j,5, Color.INDIGO);
+                setCellColor(j,6, Color.VIOLET);
+            }
+    }
 }

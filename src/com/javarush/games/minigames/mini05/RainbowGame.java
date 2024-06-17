9a0d11d6e6898003c48c08c919a05d1ca01099bc
dasama <ilrudasama@gmail.com>
2024-06-18 01:57:57 +0500

    les 20 task 5

--- src/com/javarush/games/minigames/mini05/RainbowGame.java
+++ src/com/javarush/games/minigames/mini05/RainbowGame.java
@@ -11,14 +11,14 @@ public class RainbowGame extends Game {
     @Override
     public void initialize() {
         setScreenSize(10, 7);
-            for (int j = 0; j < 10; j++){
-                setCellColor(j,0, Color.RED);
-                setCellColor(j,1, Color.ORANGE);
-                setCellColor(j,2, Color.YELLOW);
-                setCellColor(j,3, Color.GREEN);
-                setCellColor(j,4, Color.BLUE);
-                setCellColor(j,5, Color.INDIGO);
-                setCellColor(j,6, Color.VIOLET);
-            }
+        for (int j = 0; j < 10; j++) {
+            setCellColor(j, 0, Color.RED);
+            setCellColor(j, 1, Color.ORANGE);
+            setCellColor(j, 2, Color.YELLOW);
+            setCellColor(j, 3, Color.GREEN);
+            setCellColor(j, 4, Color.BLUE);
+            setCellColor(j, 5, Color.INDIGO);
+            setCellColor(j, 6, Color.VIOLET);
+        }
     }
 }

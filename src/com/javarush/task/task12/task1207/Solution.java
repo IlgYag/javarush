84d6aa0799e9f9e726a1aed032d6b62644d57314
dasama <ilrudasama@gmail.com>
2024-06-24 02:06:13 +0500

    les 22 task 7

---
+++ src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 5;
+        Integer l = 4;
+        print(i);
+        print(l);
+    }
+    public static void print(int a) {
+        System.out.println(a);
+    }
+    public static void print(Integer a) {
+        System.out.println(a);
+    }
+}

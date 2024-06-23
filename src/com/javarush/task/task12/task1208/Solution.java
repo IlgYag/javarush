e06e97ab5eaa156019ed2755d0b8c7306400f6b0
dasama <ilrudasama@gmail.com>
2024-06-24 02:06:25 +0500

    les 22 task 8

---
+++ src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+    public static void print(int i) {
+        System.out.println(i);
+    }
+    public static void print(String i) {
+        System.out.println(i);
+    }
+    public static void print(long i) {
+        System.out.println(i);
+    }
+    public static void print(boolean i) {
+        System.out.println(i);
+    }
+        public static void print(int i,int j) {
+        System.out.println(i+j);
+    }
+
+}
